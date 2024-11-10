763572a2a31d83254382804432100a969c90de4f
reflexj <simon.ksasz@gmail.com>
2024-11-10 21:27:02 +0100

    laptop

--- venv/Scripts/activate.bat
+++ venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\simon\OneDrive\Dokumente\GitHub\tutor\venv
+set VIRTUAL_ENV=C:\Users\Simon Fröhlich\Documents\GenerellVScode\FlaskApp\tutor\venv
 
 if not defined PROMPT set PROMPT=$P$G
 
